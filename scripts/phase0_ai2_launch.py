8c9643d53d9b4d713cd856c9bc95942754cf1d31
Will Merrill <willm@allenai.org>
2025-09-17 16:46:01 +0000

    Debug

--- scripts/phase0_ai2_launch.py
+++ scripts/phase0_ai2_launch.py
@@ -13,6 +13,19 @@ from olmo_core.utils import generate_uuid, prepare_cli_environment
 from olmo_core.internal.common import build_launch_config
 
 
+def get_launch_config(run_name, cluster) -> BeakerLaunchConfig:
+
+    launch_config = build_launch_config(
+        cmd=["python", "src/scripts/train/ppt2/phase0_ai2.py", "train", run_name],
+        name=run_name,
+        cluster=cluster,
+        workspace="ai2/willm-ppt2",
+        root_dir="/weka/oe-training-default/ai2-llm",
+    )
+    launch_config.allow_dirty = True
+    return launch_config
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print(f"Usage: python {sys.argv[0]} run_name cluster [OVERRIDES...]")
@@ -22,6 +35,4 @@ if __name__ == "__main__":
 
     prepare_cli_environment()
 
-    root_dir = "/weka/oe-training-default/ai2-llm"
-    cmd = sys.argv
-    build_launch_config(run_name, root_dir, cmd, cluster).launch(follow=True)
+    get_launch_config(run_name, cluster).launch(follow=True)

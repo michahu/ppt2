95c7c4fc9d60e5a9e29abb19cc27c1c55d9c592c
Will Merrill <willm@allenai.org>
2025-09-17 18:40:57 +0000

    Add custom steps to clone private repo

--- scripts/phase0_ai2_launch.py
+++ scripts/phase0_ai2_launch.py
@@ -5,24 +5,65 @@ Run this with:
     python src/scripts/train/ppt2/phase0_launch.py run01 ai2/jupiter-cirrascale-2 [OVERRIDES...]
 """
 
+from functools import lru_cache
 import sys
-from typing import List
+from typing import List, Optional
 
-from olmo_core.launch.beaker import BeakerLaunchConfig
+from beaker import Beaker, BeakerError, SecretNotFound
+
+from olmo_core.launch.beaker import BeakerLaunchConfig, BeakerEnvSecret
 from olmo_core.utils import generate_uuid, prepare_cli_environment
 from olmo_core.internal.common import build_launch_config
 
 
-def get_launch_config(run_name, cluster) -> BeakerLaunchConfig:
+# See https://github.com/allenai/OLMo-modular/blob/main/src/olmo_modular/internal/experiment.py
+# Authenticate in order to clone private repo.
+GITHUB_AUTH_STEPS = [
+    "conda install gh --channel conda-forge",
+    # assumes that conda is installed, which is true for our beaker images. # TODO: add to image
+    "echo $GITHUB_TOKEN | gh auth login --with-token",
+    # this is possibly redundant?
+]
 
+
+@lru_cache()
+def get_beaker_client() -> Optional[Beaker]:
+    try:
+        return Beaker.from_env(check_for_upgrades=False)
+    except BeakerError:
+        return None
+
+
+@lru_cache()
+def get_beaker_username() -> Optional[str]:
+    beaker = get_beaker_client()
+    if beaker is not None:
+        return beaker.account.whoami().name
+    else:
+        return None
+
+
+def get_launch_config(run_name, cluster) -> BeakerLaunchConfig:
     launch_config = build_launch_config(
-        cmd=["python", "src/scripts/train/ppt2/phase0_ai2.py", "train", run_name],
+        # Match the command that would be created with the normal launcher.
+        cmd=["src/scripts/train/ppt2/phase0_ai2.py", "train", run_name, cluster],
         name=run_name,
         cluster=cluster,
         workspace="ai2/willm-ppt2",
         root_dir="/weka/oe-training-default/ai2-llm",
     )
+
+    # Configure GitHub token secret.
+    beaker_user = get_beaker_username()
+    github_token = BeakerEnvSecret(name="GITHUB_TOKEN", secret=f"{beaker_user}_GITHUB_TOKEN")
+    launch_config.env_secrets.append(github_token)
+
+    # Add initial setup steps to authenticate GitHub CLI.
+    launch_config.setup_steps = GITHUB_AUTH_STEPS + launch_config.setup_steps
+
+    # Allow dirty state.
     launch_config.allow_dirty = True
+
     return launch_config
 
 
